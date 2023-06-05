ac12772f23b843f9794c31eb34e60fac4cb7e04a
Mvsrodri <mvsrodri@hotmail.com>
2023-06-04 22:08:21 -0300

    Segundo commit
    -criação do template do footer;
    -criação das rotas para as outras paginas;
    -finalização da estilização da pagina inicial.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
   res.render('index');
+});
+
+router.get('/descricao', function(req, res) {
+  res.render('descricao');
+});
+
+router.get('/tecnologias-e-ferramentas', function(req, res) {
+  res.render('tecnologias');
+});
+
+router.get('/desenvolvedores', function(req, res) {
+  res.render('desenvolvedores');
+});
+
+router.get('/contato', function(req, res) {
+  res.render('contato');
 });
 
 module.exports = router;
